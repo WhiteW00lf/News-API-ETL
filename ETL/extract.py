2bc611dec6ed86edfdfdc8c695fa82a767bf5986
suryapoojary-dot <surya.rubyist@gmail.com>
2025-07-04 22:13:09 +0530

    Extracted all the necessary fields from the API to dedicated lists

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,14 +1,32 @@
 from dotenv import load_dotenv
-import pandas as pd 
+import pandas as pd
 import requests
 import os
 
+article_ids = []
+titles = []
+pubdates = []
+
 load_dotenv()
-api_key = os.getenv('API_KEY')
-url=f"https://newsdata.io/api/1/latest?apikey={api_key}&q=India"
+api_key = os.getenv("API_KEY")
+url = f"https://newsdata.io/api/1/latest?apikey={api_key}&q=India"
 resp = requests.get(url)
-if(resp.status_code == 200):
+print(resp.text)
+if resp.status_code == 200:
     data = resp.json()
-    print(data)
+
 else:
     print(f"Status code - {resp.status_code} -  {resp.text}")
+
+data = resp.json()
+
+
+for each_entry in data["results"]:
+    ids = each_entry["article_id"]
+    article_ids.append(ids)
+    title = each_entry["title"]
+    titles.append(title)
+    pubdate = each_entry["pubDate"]
+    pubdates.append(pubdate)
+
+print("Finished loading the lists")
