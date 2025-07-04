dde25adf436497223ac5eef4cf1043860568ccef
suryapoojary-dot <surya.rubyist@gmail.com>
2025-07-04 22:20:09 +0530

    Transformed arrays to a dataframe with no null values.

---
+++ ETL/transform.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from extract import article_ids, titles, pubdates
+
+news_dict = {"ids": article_ids, "title": titles, "pubDate": pubdates}
+
+news_df = pd.DataFrame(news_dict)
+
+print(news_df.isna().sum())
+
+#No null values found!
